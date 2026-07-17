e869140ec64e508607bac86ea029ce6992c33aba
𓆰𓆪 <168411239+Vmallu00@users.noreply.github.com>
2026-07-17 07:29:29 +0530

    Create admin.py

---
+++ routes/admin.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+from utils import admin_required
+
+admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin_bp.route('/')
+@admin_required
+def index():
+    return render_template('admin.html')  # optional, you can expand
